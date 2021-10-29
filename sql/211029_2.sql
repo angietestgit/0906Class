1880256686fe6eee4a248d93fde33e090381748b
angietest <angietest0136@gmail.com>
2021-10-29 16:48:42 +0900

    Update 211029_2.sql

--- sql/211029_2.sql
+++ sql/211029_2.sql
@@ -18,18 +18,30 @@ WHERE price >= 20000
 ;
 
 --(3) 박지성의총구매액(박지성의고객번호는1번으로놓고작성)
-
+SELECT SUM(saleprice) AS "박지성 총구매액"
+FROM orders
+WHERE custid=1
+;
 
 --(4) 박지성이구매한도서의수(박지성의고객번호는1번으로놓고작성)
---
+SELECT SUM(custid) AS "박지성 도서의수"
+FROM orders
+WHERE custid=1
+;
 --​
 --
 --2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
 --
 --(1) 마당서점도서의총개수
---
+SELECT COUNT(bookid)
+FROM book
+;
+
 --(2) 마당서점에도서를출고하는출판사의총개수
---
+SELECT COUNT(DISTINCT publisher) AS "출판사 총개수"
+FROM book
+;
+
 --(3) 모든고객의이름, 주소
 select custname, address
 FROM customer
@@ -55,6 +67,6 @@ WHERE name LIKE '김%'
 
 --(7) 성이‘김’씨이고이름이‘아’로끝나는고객의이름과주소
 SELECT name, address
-FROM suctomer
-WHERE nema like '김%아'
+FROM customer
+WHERE name LIKE '김%아'
 ;
