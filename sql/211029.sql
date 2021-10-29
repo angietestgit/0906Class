7dc51b6232afcce11a7801f045f81a8cf3f42904
angietest <angietest0136@gmail.com>
2021-10-29 10:30:58 +0900

    Update 211029.sql

--- sql/211029.sql
+++ sql/211029.sql
@@ -78,19 +78,35 @@ WHERE mgr is null
 -- 급여 및 커미션을 기준으로 내림차순 정렬하여 표시하시오.
 SELECT ename, sal, comm
 FROM emp
-WHERE comm
+WHERE comm is not null
+ORDER BY sal, comm desc
+;
 --​
 --
 --12. 이름의 세번째 문자가 R인 사원의 이름을 표시하시오.
 --
 SELECT ename
 FROM emp
-WHERE ename like '%%R'
+WHERE ename like '__R%'
 ;
 --
 --13. 이름에 A와 E를 모두 포함하고 있는 사원의 이름을 표시하시오.
---
---14. 담당업무가 CLERK, 또는 SALESMAN이면서 급여가 $1600, $950 또는 $1300이 아닌 사원의 이름, 담당업무, 급여를 출력하시오.
---
---
+
+SELECT ename
+FROM emp
+WHERE ename like '%A%E%'
+;
+
+--14. 담당업무가 CLERK, 또는 SALESMAN이면서 급여가 $1600, $950
+--또는 $1300이 아닌 사원의 이름, 담당업무, 급여를 출력하시오.
+
+SELECT ename, job, sal
+FROM emp
+WHERE (job='CLERK' OR job='SALESMAN') AND sal NOT IN(1600,950,1300)
+;
+
 -- 15. 커미션이 $500 이상인 사원의 이름과 급여 및 커미션을 출력하시오.
+SELECT ename, sal, comm
+FROM emp
+WHERE comm>=500
+;
