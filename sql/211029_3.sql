213b2580a2a2a921a0a0dd461fb008a7159ef75c
angietest <angietest0136@gmail.com>
2021-11-01 09:59:35 +0900

    update 211029 sql

--- sql/211029_3.sql
+++ sql/211029_3.sql
@@ -63,16 +63,6 @@ SELECT TO_CHAR(SYSDATE, 'YYYY.MM.DD. HH24:MI') FROM dual;
 -- 2021.10.29. 13:00
 
 -- 숫자->문자 TO_CHAR(원본, 패턴)
-
--- 문자->날짜 TO_DATE(원본, 패턴)
---SELECT TO_DATE('2021/10/29', 'YYYY/MM/DD') FROM dual;
-SELECT TO_DATE('2021.10.29.', 'YYYY.MM.DD.') FROM dual;
-SELECT hiredate, TO_CHAR(hiredate, 'YYYY.MM.DD.') from emp; --예제
-
--- 2021.01.01~오늘 예제
-SELECT TRUNC(SYSDATE-TO_DATE('2021-01-01', 'YYYY-MM-DD')) FROM dual;  --디데이
-
--- 문자->숫자 TO_NUMBER(원본, 패턴)
 SELECT TO_CHAR(100000, '0000000') FROM dual; --빈자리를 0으로 채움
 --                      0100000
 SELECT TO_CHAR(100000.12, '0,000,000') FROM dual; --빈자리를 0으로 채움
@@ -80,3 +70,115 @@ SELECT TO_CHAR(100000.12, '0,000,000') FROM dual; --빈자리를 0으로 채움
 SELECT TO_CHAR(100000, '9,999,999') FROM dual; --9는 빈자리를 표현하지는 않음
 SELECT TO_CHAR(100000.123, 'L9,999,999.9') FROM dual; --L은 원화표시, 빈자리를 표현하지는 않음
 SELECT sal, TO_CHAR(sal*1200, 'L9,999,999') FROM emp; --예제
+
+-- 문자->날짜 TO_DATE(원본, 패턴)
+--SELECT TO_DATE('2021/10/29', 'YYYY/MM/DD') FROM dual;
+SELECT TO_DATE('2021.10.29.', 'YYYY.MM.DD.') FROM dual;
+SELECT hiredate, TO_CHAR(hiredate, 'YYYY.MM.DD.') from emp; --예제
+-- 2021.01.01~오늘 예제
+SELECT TRUNC(SYSDATE-TO_DATE('2021-01-01', 'YYYY-MM-DD')) FROM dual;  --디데이
+
+-- 문자->숫자 TO_NUMBER(원본, 패턴)
+SELECT TO_NUMBER('100,000', '9,999,999') + 100000 FROM dual; --형변환
+
+
+-- switch case 와 유사한 decode 함수
+-- emp테이블에서 사원 이름, 부서번호, 부서이름 출력
+SELECT * FROM dept;
+SELECT * FROM EMP;
+SELECT ename, deptno,
+    decode(deptno,
+        10, 'ACCOUNTING',
+        20, 'RESEARCH',
+        30, 'SALES',
+        40, 'OPERATIONS'
+    ) as dname
+FROM emp
+;
+
+
+-- 직급에 따라 급여를 인상하도록 하자.
+-- 직급이 'ANALYST'인 사원은 5%,
+--       'SALESMAN'인 사원은 10%,
+--       'CLERK'인 사원은 20% 인상한다.
+SELECT ENAME, job, sal,
+        decode(job,
+            'ANALYST', sal*1.05,
+            'SALESMAN', sal*1.1,
+            'MANAGER', sal*1.15,
+            'CLERK', sal*1.2
+        ) as upSal
+FROM emp
+;
+
+
+-- CASE WHEN THEN : if~else if 구문과 유사 -> 조건식 = 이외의 비교 연산을 할 수 있다.
+SELECT ename, sal,
+    CASE
+        WHEN sal >= 3000 THEN sal*1.3
+        WHEN sal < 3000 THEN sal*1.5
+    END AS Upsal
+FROM emp
+;
+
+
+--------------------------------------------------
+-- 집합 함수 (=그룹 함수)
+-- 하나의 행의 컬럼이 대상이 아니고, 행 그룹의 컬럼들(집합)이 대상
+--------------------------------------------------
+SELECT
+    TO_CHAR(SUM(sal)*1200, 'L999,999,999') AS "월 급여 총액",
+    TO_CHAR(ROUND(AVG(sal)*1200)) AS "월 급여 평균",
+    COUNT(*) AS "총 사원의 수",
+    COUNT(comm) AS "커미션 등록 수",
+    SUM(comm) AS "커미션 합",
+    AVG(comm) AS "커미션 평균", -- null은 평균에 들어가지 않음. 나중에 널을 따로 처리해줘야됨
+    MAX(sal) AS "가장 높은 급여",
+    MIN(sal) AS "가장 낮은 급여"
+FROM emp
+;
+
+
+-- 사원들의 업무의 개수
+SELECT COUNT(DISTINCT JOB)
+FROM emp
+;
+
+-- 각 부서별 급여의 총 합
+SELECT SUM(sal)
+FROM emp
+WHERE deptno = 10
+;
+SELECT SUM(sal)
+FROM emp
+WHERE deptno = 20
+;
+SELECT SUM(sal)
+FROM emp
+WHERE deptno = 30
+;
+
+SELECT * FROM emp ORDER BY deptno;
+
+
+-- GROUP BY : 특정 컬럼으로 그룹핑 -> 그룹 내의 평균이나 합과 같은 집합 함수를 사용할 수 있다.
+-- 부서번호를 기준으로 그룹핑
+-- -> 급여의 총합, 평균, 최대, 최소, 사원의 수, 커미션 평균, 커미션 총합
+SELECT deptno,
+       COUNT(*) AS "사원 수",
+       SUM(sal) AS "급여 총합",
+       TRUNC(AVG(sal)) AS "급여 평균",
+       MAX(sal) AS "최대 급여",
+       MIN(sal) AS "최소 급여",
+       COUNT(comm) AS "커미션 대상자 수",
+       NVL(SUM(comm),0) AS "커미션 총합",
+       NVL(AVG(comm),0) AS "커미션 평균"
+FROM emp
+-- WHERE deptno != 20 --20번 부서만 뺌
+GROUP BY deptno
+-- HAVING AVG(sal) >= 2000 -- 지표같은거 써야될때 잘 쓰임! 해빙함수
+HAVING MAX(sal) > 2900
+ORDER BY deptno
+;
+
+
--- sql/211029_4.sql
+++ sql/211029_4.sql
@@ -83,22 +83,57 @@ SELECT
 FROM emp
 ;
 --28. 직급별 사원의 최저 급여를 출력하시오. 관리자를 알 수 없는 사원과
--- 최저 급여가 2000 미만인 그룹은 제외시키고 결과를 급여에 대한 내림차순으로 정렬하여 출력하시오.
+-- 최저 급여가 2000 미만인 그룹은 제외시키고
+-- 결과를 급여에 대한 내림차순으로 정렬하여 출력하시오.
 SELECT job,
     MIN(sal) AS "직급별최저급여"
 FROM emp
---WHERE MIN(sal) >= 2000
+WHERE mgr IS NOT NULL AND sal >= 2000
 GROUP BY job
-ORDER BY sal DESC
+ORDER BY MIN(sal) DESC
 ;
 --
 --29. 각 부서에 대해 부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오.
 -- 평균 급여는 소수점 둘째 자리로 반올림 하시오.
---
---30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오. 평균 급여는 정수로 반올림 하시오. DECODE 사용.
---
---31. 업무를 표시한 다음 해당 업무에 대해 부서 번호별 급여 및 부서 10, 20, 30의 급여 총액을 각각 출력하시오.
+SELECT deptno AS "부서 번호",
+    COUNT(ename) AS "사원 수",
+    ROUND(AVG(sal), 2) AS "평균 급여"
+FROM emp
+GROUP BY deptno
+;
+
+--30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의
+--모든 사원의 평균 급여를 출력하시오. 평균 급여는 정수로 반올림 하시오. DECODE 사용.
+SELECT deptno,
+    DECODE( --부서번호 이름
+        10, 'ACCOUNTING',
+        20, 'RESEARCH',
+        30, 'SALES',
+        40, 'OPERATIONS'
+    ) AS dname,
+    DECODE( --지역명
+        10, 'A',
+        20, 'B',
+        30, 'C',
+        40, 'D'
+    ) AS "지역 명",
+        COUNT(*) AS "사원 수",
+        ROUND(AVG(sal), 2) AS "평균 급여"
+FROM emp
+GROUP BY deptno
+;
+
+--31. 업무를 표시한 다음 해당 업무에 대해 부서 번호별 급여
+-- 및 부서 10, 20, 30의 급여 총액을 각각 출력하시오.
 -- 별칭은 각 job, dno, 부서 10, 부서 20, 부서 30, 총액으로 지정하시오. 
 --( hint. Decode, group by )
 --
---​
+SELECT job, deptno AS "dno",
+        DECODE(deptno, 10, SUM(sal)) AS "부서 10",
+        DECODE(deptno, 20, SUM(sal)) AS "부서 20",
+        DECODE(deptno, 30, SUM(sal)) AS "부서 20",
+    SUM(sal) AS "총액"
+FROM emp
+GROUP BY job, deptno
+ORDER BY deptno;
+    
